4f90d2cbf269ac46b4a66366e334177a8789a6f8
Karine Lima <karine.elima73@gmail.com>
2025-05-17 12:27:16 -0300

    aprimorando o código

--- provas/trits.c
+++ provas/trits.c
@@ -43,7 +43,10 @@ int main(){
 	
 	if(dig0 == 1){
 		dig0 = 0;
-	} else if (dig0 == 0){
+	} else if (dig0 == 0 && dig1 == 0 && dig2 == 0 && dig3 == 0){
+		dig0 == 0;
+	}
+	else if (dig0 == 0){
 		dig0 = -1;
 	} else {
 		dig0 = 1;
