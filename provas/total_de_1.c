a658580c55586b9b43475f4e16e2fb28b8df682e
Karine Lima <karine.elima73@gmail.com>
2025-05-19 14:12:33 -0300

    ...

--- provas/total_de_1.c
+++ provas/total_de_1.c
@@ -19,12 +19,7 @@ int main(){
 	bit6 = numero % 2;
 	
 	total_de_1 = bit1 + bit2 + bit3 + bit4 + bit5 + bit6;
-	/*total_de_1 += (bit1 == 1)? 1 : 0;
-	total_de_1 += (bit2 == 1)? 1 : 0;
-	total_de_1 += (bit3 == 1)? 1 : 0;
-	total_de_1 += (bit4 == 1)? 1 : 0;
-	total_de_1 += (bit5 == 1)? 1 : 0;
-	total_de_1 += (bit6 == 1)? 1 : 0;*/
+	
 	printf("O total de bits igual a 1 é %d.", total_de_1);
 	
 	return 0;
