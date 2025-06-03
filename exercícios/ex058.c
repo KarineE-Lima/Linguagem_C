5d3e3161128586d81e5a5c979201968c206fc7b1
Karine Lima <karine.elima73@gmail.com>
2025-06-03 16:40:44 -0300

    Update ex058.c

--- exercícios/ex058.c
+++ exercícios/ex058.c
@@ -9,9 +9,8 @@ int main(){
 	scanf("%d", &qtdTermos);
 	/*for (contador = 1; contador <= qtdTermos; contador++){
 		fatorial = 1.0;
-		for (mult = 1; mult <= contador; mult++){
+		for (mult = 1; mult <= contador; mult++)
 			fatorial *= mult;
-		}
 		euler +=  (1.0 / fatorial);
 	}
 	printf("%f", euler);*/
