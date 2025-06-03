6e29b2f72ff7bdb97776713520bf00ea77b7fbc6
Karine Lima <karine.elima73@gmail.com>
2025-06-03 12:40:02 -0300

    Update ex058.c

--- exercícios/ex058.c
+++ exercícios/ex058.c
@@ -2,18 +2,23 @@
 
 int main(){
 	int qtdTermos, contador, mult;
-	float fatorial;
+	float fatorial = 1.0;
 	float euler = 1.0;
 	
 	printf("Insira a quantidade de termos: ");
 	scanf("%d", &qtdTermos);
-	for (contador = 1; contador <= qtdTermos; contador++){
+	/*for (contador = 1; contador <= qtdTermos; contador++){
 		fatorial = 1.0;
 		for (mult = 1; mult <= contador; mult++){
 			fatorial *= mult;
 		}
 		euler +=  (1.0 / fatorial);
 	}
-	printf("%f", euler);
+	printf("%f", euler);*/
+	for(contador = 1; contador <= qtdTermos; contador++){
+		fatorial *= contador;
+		euler += (1.0 / fatorial);
+		printf("Termo %d: %f\n", contador, euler);
+	}
 
 }
