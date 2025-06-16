f8498d24a9f0807c3a1d58091a5c875dcb3c761b
Karine Lima <karine.elima73@gmail.com>
2025-06-16 11:26:10 -0300

    Update execicioSALAS.c

--- exercícios/execicioSALAS.c
+++ exercícios/execicioSALAS.c
@@ -3,25 +3,17 @@
 
 int main(){
 	int sala[TAM_VET] = {50, 42, -35, 2, -60, 5, 30, -1, 40, 31};
-	int indice, vidas1, vidas2, maiorvida;
-	vidas1 = 0;
-	vidas2 = 0;
-	if(sala[0]> 0){
-		for(indice = 0; indice < TAM_VET; indice++){
-			if(indice < TAM_VET/2)
-				vidas1 += sala[indice];
-			if(indice >= TAM_VET/2)
-				vidas2 += sala[indice];
+	int indice, salaEntrada, salaSaida, maiorvida = -1, vidaAtual;
+	
+	for(salaEntrada = 0; salaEntrada < TAM_VET; salaEntrada++){
+		for(salaSaida = salaEntrada; salaSaida < TAM_VET; salaSaida++){
+			for(indice = salaEntrada, vidaAtual = 0; indice <= salaSaida; indice++){
+				vidaAtual += sala[indice];
+			}
 		}
-		maiorvida = (vidas1 > vidas2)? vidas1 : vidas2;
-	} else {
-		for(indice = 1; indice < TAM_VET; indice++){
-			if(indice < ((TAM_VET/2) + 1))
-				vidas1 += sala[indice];
-			if(indice >= TAM_VET/2)
-				vidas2 += sala[indice];
-		}
-		maiorvida = (vidas1 > vidas2)? vidas1 : vidas2;
+		if(vidaAtual > maiorvida)
+			maiorvida = vidaAtual;
+
 	}
 	printf("%d", maiorvida);
 	
