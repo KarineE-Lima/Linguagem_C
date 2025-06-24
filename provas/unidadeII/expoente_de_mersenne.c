0e5d12788d95dc3084a7044f4e3d2438015fabfc
Karine Lima <karine.elima73@gmail.com>
2025-06-24 14:18:30 -0300

    Update expoente_de_mersenne.c

--- provas/unidadeII/expoente_de_mersenne.c
+++ provas/unidadeII/expoente_de_mersenne.c
@@ -1,37 +1,25 @@
 #include <stdio.h>
 #include <locale.h>
-#define TAM_VET 20
+#define QTD_EXP 8
 
 int main(){
 	setlocale(LC_ALL, "Portuguese");
 	
-	int exMersenne[TAM_VET];
-	int nMersenne[TAM_VET];
-	int numero, cont;
-	int expoente = 2, indice = 0, divisor, divisoes;
-	while(indice < TAM_VET){
+	int expoente = 1, numero, indice = 0, contador, divisor, divisoes;
+	
+	while(indice < QTD_EXP){
+		numero = 2;
+		for(contador = 1; contador < expoente; contador++)
+			numero *= 2;
+		numero -= 1;
 		divisoes = 0;
-		for(divisor = 1; divisor <= expoente; divisor++)
-			divisoes += (expoente % divisor == 0)? 1 : 0;
-		
+		for(divisor = 1; divisor <= numero; divisor++)
+			divisoes += (numero % divisor == 0) ? 1 : 0;
 		if(divisoes == 2){
-			exMersenne[indice] = expoente;
+			printf("%d = %d\n", expoente, numero);
 			indice++;
 		}
 		expoente++;
 	}
-	
-	for(indice = 0; indice < TAM_VET; indice++){
-		numero = 2;
-		for(cont = 1; cont < exMersenne[indice]; cont++){
-			numero *= 2;
-		}
-		nMersenne[indice] = numero - 1;
-	}
-	
-	for(indice = 0; indice < TAM_VET; indice++){
-		printf("%d (2 ^ %d - 1)\n", nMersenne[indice], exMersenne[indice]);
-	}
-	
 	return 0;
 }
