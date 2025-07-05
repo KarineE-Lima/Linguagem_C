168e2b021c8a9bfae5eb946416d5c4b124dc01b8
Karine Lima <karine.elima73@gmail.com>
2025-07-05 19:09:23 -0300

    Update lista_de_presenca-2014.2.c

--- provas/unidadeII/lista_de_presenca-2014.2.c
+++ provas/unidadeII/lista_de_presenca-2014.2.c
@@ -4,30 +4,29 @@
 int main()
 {
     int lista[TAM_LISTA];
-    int indice, qtd_alunos, cont, repetido;
+    int nova_lista[TAM_LISTA];
+    int indice, qtd_alunos, cont, aluno;
     
-    for(indice = 0, qtd_alunos = 0; indice < TAM_LISTA; indice++, qtd_alunos++){
+    for(indice = 0; indice < TAM_LISTA; indice++){
         printf("Insira o número do aluno: ");
         scanf("%d", &lista[indice]);
     }
-    for(indice = 0; indice < qtd_alunos; indice++){
-        for(cont = indice + 1; cont < qtd_alunos; cont++){
-            if(lista[cont] == lista[indice]){
-                qtd_alunos--;
-                for(repetido = cont; repetido < qtd_alunos; repetido++){
-                    if(lista[repetido] == lista[repetido + 1]){
-                        lista[repetido] = lista[repetido + 2];
-                        qtd_alunos--;
-                    } else{
-                        lista[repetido] = lista[repetido + 1];
-                    }
-                }
-            }
+    for(indice = 0; indice < TAM_LISTA; indice++){
+        aluno = lista[indice];
+        for(cont = indice + 1; cont < TAM_LISTA; cont++){
+            if(lista[cont] == aluno)
+                lista[cont] = 0;
         }
-    } printf("Quantidade de alunos: %d = ", qtd_alunos);
-    for(indice = 0; indice < qtd_alunos; indice++){
-        printf("%d ", lista[indice]);
+    } 
+    for(indice = 0, qtd_alunos = 0; indice < TAM_LISTA; indice++){
+        if(lista[indice] != 0){
+            nova_lista[qtd_alunos] = lista[indice];
+            qtd_alunos++;
+        }
     }
+    printf("%d alunos\n", qtd_alunos);
+    for(indice = 0; indice < qtd_alunos; indice++)
+        printf("%d - %d\n", indice + 1, nova_lista[indice]);
 
     return 0;
 }
