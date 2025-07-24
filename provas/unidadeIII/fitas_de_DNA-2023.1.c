daa97f8ee5bc82732adf822b7ae26df7b6accebd
Karine Lima <karine.elima73@gmail.com>
2025-07-24 16:00:18 -0300

    Update fitas_de_DNA-2023.1.c

--- provas/unidadeIII/fitas_de_DNA-2023.1.c
+++ provas/unidadeIII/fitas_de_DNA-2023.1.c
@@ -3,35 +3,39 @@
 #define QTD 5
 
 int main(){
-	char fitas[QTD][TAM_STR];
+	char fitas[QTD][TAM_STR] = {"CGGATTTT",
+								"CGGAAATT",
+								"GCGTTAAT", 
+								"GCGTTTTT", 
+								"ACGGACAT"};
 	char consenso[TAM_STR];
 	int icont, jcont, kcont, repet, mais_rep;
 	char mais_oc, atual;
 	
-	for(icont = 0; icont < QTD; icont++){
+	/*for(icont = 0; icont < QTD; icont++){
 		printf("Insira as enzimas da %dª fita: ", icont + 1);
 		fgets(fitas[icont], TAM_STR, stdin);
 		fflush(stdin);
-	}
-	icont = 0;
-	for(jcont = 0; fitas[icont][jcont] != '\0' && fitas[0][jcont] != '\n'; jcont++){
+	}*/
+	
+	for(jcont = 0; jcont < TAM_STR ; jcont++){
 		for(icont = 0, mais_rep = 0; icont < QTD; icont++){
 			atual = fitas[icont][jcont];
-			for(kcont = 0, repet = 0; kcont < QTD; kcont++){
+			for(kcont = icont, repet = 0; kcont < QTD; kcont++){
 				if(atual == fitas[kcont][jcont]){
 					repet++;
 				}
 			}
-			if(repet == mais_rep){
-				mais_oc = '?';
-			} else if(repet > mais_rep){
-				repet = mais_rep;
+			if(repet > mais_rep){
+				mais_rep = repet;
 				mais_oc = atual;
+			} else if(repet == mais_rep){
+				mais_oc = '?';
 			}
 		}
 		consenso[jcont] = mais_oc;
 	}
-	consenso[jcont++] = '\0';
+	//consenso[jcont++] = '\0';
 	puts(consenso);
 	
 	return 0;
